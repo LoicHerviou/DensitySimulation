a67e50c55acd19d98e804e7e0693e9936c405a85
herviou <you@example.com>
2018-05-30 18:28:39 +0200

    Update of Evolution and bug fixes for even length

--- Suzuki-Trotter.jl
+++ Suzuki-Trotter.jl
@@ -19,41 +19,41 @@ indexMaxTER is the rightmost site in matRho
 Traces are the trace matrix for extendedRho
 TracesSmall are the trace matrix for matRho
 """
-function joinDensityMatrix!(extendedRho, matRho, indexMin, indexMinTER, indexMax, indexMaxTER, Traces, TracesSmall)
+function joinDensityMatrix!(extendedRho, matRho, indexMin, indexMinTER, indexMax, indexMaxTER, Traces, TracesSmall, invMatJoin, pseudoIdentity)
     if indexMin==indexMinTER && indexMax==indexMaxTER
         if mod(length(matRho), 2)==1
-            println("Bug1 in joinDensityMatrix")
+            println("Bug1 in joinDensityMatrix 1")
         end
         for l in 1:2:length(matRho)-1
-            joinDensityMatAux!(extendedRho[(l+1)÷2], matRho[l], matRho[l+1], Traces, TracesSmall)
+            joinDensityMatAux!(extendedRho[(l+1)÷2], matRho[l], matRho[l+1], Traces, TracesSmall, invMatJoin, pseudoIdentity)
         end
     elseif indexMin==indexMinTER && indexMax==indexMaxTER-1
         if mod(length(matRho), 2)==0
-            println("Bug2 in joinDensityMatrix")
+            println("Bug2 in joinDensityMatrix 2")
         end
         for l in 1:2:length(matRho)-1
-            joinDensityMatAux!(extendedRho[(l+1)÷2], matRho[l], matRho[l+1], Traces, TracesSmall)
+            joinDensityMatAux!(extendedRho[(l+1)÷2], matRho[l], matRho[l+1], Traces, TracesSmall, invMatJoin, pseudoIdentity)
         end
         rightFusionIdentity!(extendedRho[end], matRho[end])
     elseif indexMin==indexMinTER+1 && indexMax==indexMaxTER
         if mod(length(matRho), 2)==0
-            println("Bug3 in joinDensityMatrix")
+            println("Bug3 in joinDensityMatrix 3")
         end
         for l in 2:2:length(matRho)-1
-            joinDensityMatAux!(extendedRho[l÷2+1], matRho[l], matRho[l+1], Traces, TracesSmall)
+            joinDensityMatAux!(extendedRho[l÷2+1], matRho[l], matRho[l+1], Traces, TracesSmall, invMatJoin, pseudoIdentity)
         end
         leftFusionIdentity!(extendedRho[1], matRho[1])
     elseif indexMin==indexMinTER+1 && indexMax==indexMaxTER-1
         if mod(length(matRho), 2)==1
-            println("Bug4 in joinDensityMatrix")
+            println("Bug4 in joinDensityMatrix 4")
         end
         for l in 2:2:length(matRho)-1
-            joinDensityMatAux!(extendedRho[l÷2+1], matRho[l], matRho[l+1], Traces, TracesSmall)
+            joinDensityMatAux!(extendedRho[l÷2+1], matRho[l], matRho[l+1], Traces, TracesSmall, invMatJoin, pseudoIdentity)
         end
         leftFusionIdentity!(extendedRho[1], matRho[1])
         rightFusionIdentity!(extendedRho[end], matRho[end])
     else
-        println("Bug in joinDensityMatrix!")
+        println("Bug in joinDensityMatrix! 5")
     end
 end
 
@@ -67,7 +67,7 @@ rhoRight is the initial right matrix
 Traces are the trace matrix for extendedRho
 TracesSmall are the trace matrix for matRho
 """
-function joinDensityMatAux!(rhoGuess, rhoLeft, rhoRight, Traces, TracesSmall)
+function joinDensityMatAux!(rhoGuess, rhoLeft, rhoRight, Traces, TracesSmall, invMatJoin, pseudoIdentity)
     len=getLength(length(rhoLeft));
     # maxinconsistency=1e-8
     # consistency=max(norm(reduceDensityMatrixRight(rhoGuess, Traces)-rhoLeft, 2), norm(reduceDensityMatrixLeft(rhoGuess, Traces)-rhoRight, 2)) ###Is it needed ?
@@ -117,12 +117,14 @@ end
 
 Let the extended matrix evolve under Uleft and its transconjugate. ToDO: improve
 """
-function evolveMatrix!(extendedRho, ULeft, tULeft)
+function evolveMatrix!(extendedRho,temp,  ULeft, tULeft)
     lenmat=size(ULeft)[1];
     for l in 1:length(extendedRho)
-        extendedRho[l]=reshape(ULeft*reshape(extendedRho[l], (lenmat, lenmat))*tULeft, lenmat*lenmat)
+        A_mul_B!(temp,ULeft, reshape(view(extendedRho[l], :), (lenmat, lenmat)))
+        A_mul_B!(reshape(view(extendedRho[l], :), (lenmat, lenmat)), temp, tULeft)
     end
 end
+
 
 """
     massReduce!(matRho, timeEvolvedRho, indexmin, indexminTER, Traces)
@@ -238,34 +240,39 @@ end
 ###################
 function main(hx::Float64, hz::Float64, T::Float64; maxLen=3, dt=0.0001)
     #Define Pauli Matrices
-    sx=[0 1; 1 0]; sz=[1  0 ; 0 -1]; sy=[0 -1im; 1im 0]
+    sx=[0 1; 1 0]; sz=-[1  0 ; 0 -1]; sy=[0 -1im; 1im 0]
     tim=dt:dt:T
     #Define Hamiltonian
     dicLocalHamiltonianInside=buildLocalHamiltonian(hx, hz, maxLen)  #### Build local Hamiltonian i:i+1 for i=0:maxLen-1 for maxLen+1 sites matrices.
     dicLocalHamiltonian=buildLocalHamiltonian(hx, hz, maxLen+1)  #### Build local Hamiltonian i:i+1 for i=0:maxLen-1 for maxLen+1 sites matrices.
     ###Evolution operator for even and odd sites
     ###toDo: reexpress as evolution on vectors
-    ULeft=expm(full(-1im*dt*sum([dicLocalHamiltonian[x] for x in 1:2:maxLen+1])))
+    ULeft=expm(full(-1im*dt*sum([dicLocalHamiltonian[x] for x in 1:2:maxLen])))
     tULeft=ULeft'
     #StockedOperator
-    extendedRho_o=Array{Array{Complex128, 1}}((maxLen+1)÷2); extendedRho_e=Array{Array{Complex128, 1}}((maxLen+1)÷2);
-    for l in 1:length(extendedRho_e)
+    extendedRho_o=Array{Array{Complex128, 1}}((maxLen+1)÷2); extendedRho_e=Array{Array{Complex128, 1}}((maxLen)÷2+1);
+    for l in 1:length(extendedRho_o)
         extendedRho_o[l]=reshape(eye(2^(maxLen+1), 2^(maxLen+1))/2^(maxLen+1),  2^(maxLen*2+2))
+    end
+    for l in 1:length(extendedRho_e)
         extendedRho_e[l]=reshape(eye(2^(maxLen+1), 2^(maxLen+1))/2^(maxLen+1),  2^(maxLen*2+2))
     end
     ###Setting up the global trace operators: note, the original one site reduced DM is marginally faster
     ###Choosing global matrixes seem to slow down code by ~1% but is much easier to read. More benchmark to be done later ?
     Traces=initializeTraceMatrices(maxLen+1);
     TracesSmall=initializeTraceMatrices(maxLen);
-    if !isdefined(:MatJoin)
-        TrRight=buildTraceMatrixRight(maxLen);
-        Tr=buildTraceMatrix(maxLen);
-        TrLeft=buildTraceMatrixLeft(maxLen);
-        global const MatJoin=transpose(sparse(vcat(TrRight, TrLeft, Tr)));
-        global const invMatJoin=sparse(triming.(pinv(full(MatJoin))));
-        global const pseudoIdentity=sparse(triming.(MatJoin*invMatJoin));
-    end
-    #vectKernelMatJoin=triming.(nullspace(full(MatJoin)));
+    #if !isdefined(:MatJoin)
+    TrRight=buildTraceMatrixRight(maxLen);
+    Tr=buildTraceMatrix(maxLen);
+    TrLeft=buildTraceMatrixLeft(maxLen);
+    MatJoin=transpose(sparse(vcat(TrRight, TrLeft, Tr)));
+    invMatJoin=sparse(triming.(pinv(full(MatJoin))));
+    pseudoIdentity=sparse(triming.(MatJoin*invMatJoin));
+    #end
+    ###For memory efficience
+    tempMatrix=reshape(similar(extendedRho_e[1]), (size(ULeft)[1],size(ULeft)[1]) )
+
+
     ###Observables###
     tim2=0:100*dt:T;
     szObservable=zeros((length(tim)+99)÷100+1);
@@ -302,12 +309,12 @@ function main(hx::Float64, hz::Float64, T::Float64; maxLen=3, dt=0.0001)
             sxObservable[cnt2]=real(trace(sx*reshape(buildReducedDensityMatrix(matRho[1-minMatRho], 0, TracesSmall), (2, 2))))
             cnt2+=1
         end
-        joinDensityMatrix!(extendedRho_o, matRho, minMatRho, indices_o[1], maxMatRho, indices_o[2], Traces, TracesSmall);   #Build the larger density Matrix
-        evolveMatrix!(extendedRho_o, ULeft, tULeft);   #Evolve the larger densityMatrix
+        joinDensityMatrix!(extendedRho_o, matRho, minMatRho, indices_o[1], maxMatRho, indices_o[2], Traces, TracesSmall, invMatJoin, pseudoIdentity);   #Build the larger density Matrix
+        evolveMatrix!(extendedRho_o, tempMatrix, ULeft, tULeft);   #Evolve the larger densityMatrix
         massReduce!(matRho, extendedRho_o, minMatRho, indices_o[1], Traces) #Compute the update densityMatrix
         minMatRho, maxMatRho=extendZone!(matRho, minMatRho, maxMatRho, extendedRho_o, indices_o, extendedRho_e, indices_e, Traces, TracesSmall)
-        joinDensityMatrix!(extendedRho_e, matRho, minMatRho, indices_e[1], maxMatRho, indices_e[2], Traces, TracesSmall)   #Build the larger density Matrix
-        evolveMatrix!(extendedRho_e, ULeft, tULeft)
+        joinDensityMatrix!(extendedRho_e, matRho, minMatRho, indices_e[1], maxMatRho, indices_e[2], Traces, TracesSmall, invMatJoin, pseudoIdentity)   #Build the larger density Matrix
+        evolveMatrix!(extendedRho_e, tempMatrix, ULeft, tULeft)
         massReduce!(matRho, extendedRho_e, minMatRho, indices_e[1], Traces)
         minMatRho, maxMatRho=extendZone!(matRho, minMatRho, maxMatRho, extendedRho_e, indices_e, extendedRho_o, indices_o, Traces, TracesSmall)
         ##Measure sz
